59c61fa997c0ed252e634e94756e02d2cd0fad56
Emre <118940184+Emre033@users.noreply.github.com>
2023-06-04 03:12:09 +0300

    Create devpara.js

---
+++ komutlar/devpara.js
@@ -0,0 +1,17 @@
+// BU ALTYAPIYI İZİNSİZ PAYLAŞMAK YASAKTIR
+module.exports = {
+name:"devpara",
+code:`
+$color[1;RANDOM]
+$setGlobalUserVar[param;$sum[$getGlobalUserVar[param;$authorID];1000000];$authorID]
+$author[1;$userTag;$userAvatar[$authorID]]
+$title[1;$userTag[$clientID] | Developer Para]
+$color[1;RANDOM]
+$description[1;Geliştiricim Olduğun İçin **1M** Coin Kazandın.]
+$footer[1;$userTag[$clientID] Tüm Hakları Saklıdır.]
+$onlyForIDs[KENDİ ID'N;]
+
+
+`
+  }
+// BU ALTYAPIYI İZİNSİZ PAYLAŞMAK YASAKTIR
