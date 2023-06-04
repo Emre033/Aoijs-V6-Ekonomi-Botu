5eecd4d481a73218f7237b28c107eed8d87a59f4
Emre <118940184+Emre033@users.noreply.github.com>
2023-06-04 03:11:12 +0300

    Update eval.js

--- komutlar/eval.js
+++ komutlar/eval.js
@@ -1,1 +1,10 @@
+module.exports = {
+  name:"eval",
+  code:`
+  $eval[$message]
+  $onlyForIDs[KENDİ İDN;Sahibim Değilsin]
 
+
+  `}
+//KOD DENEMENİZE YARAR HER KOMUTU ÇALIŞTIRMAZ!
+// BU ALTYAPIYI İZİNSİZ PAYLAŞMAK YASAKTIR
