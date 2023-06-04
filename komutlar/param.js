be671a67d9d7ecce87e68689a194ed1bd5deded9
Emre <118940184+Emre033@users.noreply.github.com>
2023-06-04 03:17:09 +0300

    Create param.js

---
+++ komutlar/param.js
@@ -0,0 +1,9 @@
+module.exports = {
+name:"param",
+code: `
+$title[1;$userTag[$clientID] | Param]
+$description[1;Şuanda $getGlobalUserVar[param;$authorID] Coinin Mevcut!
+$footer[1;$userTag Tüm Hakları Saklıdır.]
+`
+// BU ALTYAPIYI İZİNSİZ PAYLAŞMAK YASAKTIR
+}
