217d9f02e27cbf250c47bdcf33aa279a86801f41
Emre <118940184+Emre033@users.noreply.github.com>
2023-06-04 03:15:03 +0300

    Update haftalik.js

--- komutlar/haftalik.js
+++ komutlar/haftalik.js
@@ -1,1 +1,10 @@
-
+module.exports = {
+name: "Haftalık",
+description:"Haftalik",
+code: `
+$title[1;$userTag[$clientID] | Günlük]
+$description[1;
+Haftalık Ödülün Verildi ve $random[500;4000] Coin Kazandın.
+$setGlobalUserVar[param;$sum[$getGlobalUserVar[param;$authorID];$random[200;5000]];$authorID]
+$footer[1;$userTag[$clientID] Tüm Hakları Saklıdır.]
+$globalUserCooldown[7d;1 Hafta Dolmadı=)]`}
