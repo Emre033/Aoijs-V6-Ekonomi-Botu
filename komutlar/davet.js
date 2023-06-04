8e4968c528e058ed4912587c6e76a4002a5496cd
Emre <118940184+Emre033@users.noreply.github.com>
2023-06-04 03:13:01 +0300

    Create davet.js

---
+++ komutlar/davet.js
@@ -0,0 +1,13 @@
+// BU ALTYAPIYI İZİNSİZ PAYLAŞMAK YASAKTIR
+module.exports = {
+name:"davet",
+code:`
+$color[1;RANDOM]
+$title[1;$userTag[$clientID] | Davet]
+$description[1;Aşağıdaki Butonlar Aracılığıyla Beni Sunucuna Ekleyebilir veya Destek Sunucuma Erişebilirsin!]
+$footer[1;$userTag[$clientID] Tüm Hakları Saklıdır.]
+$addTimestamp[1]
+$addButton[1;Davet Et;link;https://discord.com/api/oauth2/authorize?client_id=$clientID&permissions=8&scope=bot;no] 
+
+`
+} // BU ALTYAPIYI İZİNSİZ PAYLAŞMAK YASAKTIR
